fa5aaf3257e63d9a6be9d4378573f6e1c62121ee
willkj <wdwill55@gmail.com>
2024-05-15 15:24:29 -0400

    commit

--- escola/cursos/urls.py
+++ escola/cursos/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import CursosAPIView, AvaliacoesAPIView
+from .views import CursoAPIView, CursosAPIView, AvaliacoesAPIView, AvaliacaoAPIView
 
 urlpatterns = [
     path('cursos/', CursosAPIView.as_view(), name='cursos'),
-    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes')
+    path('cursos/<int:pk>', CursoAPIView.as_view(), name='curso'),
+    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
+    path('avaliacoes/<int:pk>', AvaliacaoAPIView.as_view(), name='avaliacao')
+
 ]
--- escola/cursos/views.py
+++ escola/cursos/views.py
@@ -7,16 +7,15 @@ class CursosAPIView(generics.ListCreateAPIView):
     queryset= Curso.objects.all()
     serializer_class = CursoSerializer
 
-#class CursoAPIView():
-
-
-# class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
 
     
 class AvaliacoesAPIView(generics.ListCreateAPIView):
     queryset= Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
 
-# class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
-
-
+class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
